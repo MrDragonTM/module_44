33fd913f15edc85f139d61d8f6a376ca6463557a
Егор <egorkomarov700@gmail.com>
2023-08-21 11:26:57 +0300

    les9

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -4,13 +4,14 @@ from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
     
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 
+                    'created_date', 'updated_date', 'auction', 'image']
     list_filter = ['auction', 'created_at']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         (
             'Общее', {
-                'fields': ('title', 'description')
+                'fields': ('title', 'description', 'image')
             }
         ),
 
